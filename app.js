64d4d6e7af60cdb33bcf6c3f385fbf2b55a93e43
Anmol-Mittal30 <mittalanmol0309@gmail.com>
2025-09-02 20:35:00 +0530

    Add MongoUrl

--- app.js
+++ app.js
@@ -14,7 +14,8 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 
 
-const Mongo_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const Mongo_URL = process.env.MONGO_URL || "mongodb://127.0.0.1:27017/wanderlust";
+
 
 main()
     .then(() => {
