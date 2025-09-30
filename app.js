68774aaf6ddf545a30aed9d9b895d997851be773
Anmol-Mittal30 <mittalanmol0309@gmail.com>
2025-09-30 17:17:12 +0530

    removing helmet

--- app.js
+++ app.js
@@ -54,62 +54,6 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
-// In app.js
-
-// ---------------------- HELMET SECURITY MIDDLEWARE (CORRECTED) ---
-const scriptSrcUrls = [
-    "https://stackpath.bootstrapcdn.com/",
-    "https://api.tiles.mapbox.com/",
-    "https://api.mapbox.com/",
-    "https://kit.fontawesome.com/",
-    "https://cdnjs.cloudflare.com/",
-    "https://cdn.jsdelivr.net",
-    "https://www.paypal.com", // Added for PayPal script
-];
-const styleSrcUrls = [
-    "https://kit-free.fontawesome.com/",
-    "https://stackpath.bootstrapcdn.com/",
-    "https://api.mapbox.com/",
-    "https://api.tiles.mapbox.com/",
-    "https://fonts.googleapis.com/",
-    "https://use.fontawesome.com/",
-    "https://cdn.jsdelivr.net",
-];
-const connectSrcUrls = [
-    "https://api.mapbox.com/",
-    "https://a.tiles.mapbox.com/",
-    "https://b.tiles.mapbox.com/",
-    "https://events.mapbox.com/",
-    "https://www.paypal.com", // Added for PayPal API calls
-];
-// ✅ THE FIX IS HERE
-const fontSrcUrls = ["https://fonts.gstatic.com"];
-
-app.use(
-    helmet.contentSecurityPolicy({
-        directives: {
-            defaultSrc: [],
-            connectSrc: ["'self'", ...connectSrcUrls],
-            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
-            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
-            workerSrc: ["'self'", "blob:"],
-            objectSrc: [],
-            imgSrc: [
-                "'self'",
-                "blob:",
-                "data:",
-                "https://res.cloudinary.com/dhpaiyp7b/",
-                "https://images.unsplash.com/",
-            ],
-            // ✅ THE FIX IS APPLIED HERE
-            fontSrc: ["'self'", ...fontSrcUrls],
-            // Added for PayPal iFrame
-            frameSrc: ["'self'", "https://www.paypal.com"],
-        },
-    })
-);
-// ---------------------- END OF HELMET CONFIG ---------------------
-
 
 
 const store = MongoStore.create({
