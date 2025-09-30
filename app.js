87560cb134dd3f055813825be585e2a22aa89c80
Anmol-Mittal30 <mittalanmol0309@gmail.com>
2025-09-30 17:27:02 +0530

    fix public folder

--- app.js
+++ app.js
@@ -48,13 +48,11 @@ app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
 
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); // <-- ADD THIS LINE HERE
 app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
-app.use(express.static(path.join(__dirname, "/public")));
-
-
 
 const store = MongoStore.create({
     mongoUrl: Mongo_URL,
